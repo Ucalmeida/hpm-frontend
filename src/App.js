574b603ad2808476f9a88b2f063d9da9d93cea08
93118180544 <urian.castro@gmail.com>
2021-03-17 20:43:46 -0300

    Alteração do package.json, do App.js e importação do adminlte.portal.css para index.scss. Inserção da div login-page em Login.jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import Login from './modules/Login';
 import Principal from './modules/principal';
+
 //JS
 import 'jquery/dist/jquery.min';
-import 'admin-lte/dist/js/adminlte.min.js';
 import 'overlayscrollbars-react/dist/overlayscrollbars-react';
 
 class App extends Component {
--- src/modules/Login.jsx
+++ src/modules/Login.jsx
@@ -4,53 +4,55 @@ document.getElementById('root').classList = 'hold-transition login-page';
 
 const Login = () => {
     return (
-        <div className="login-box">
-            <div className="card card-outline card-primary">
-                <div className="card-header text-center">
-                        <b>PORTAL</b>
-                        <span> HPM</span>
-                </div>
-                <div className="card-body login-card-body">
-                    <p className="login-box-msg">Insira abaixo suas credenciais para entrar no módulo HPM</p>
-                    <form >
-                        <div className="mb-3">
-                            <div className="input-group">
-                                <input
-                                    type="email"
-                                    className="form-control"
-                                    placeholder="Usuário"
-                                />
-                                <div className="input-group-append">
-                                    <div className="input-group-text">
-                                        <span className="fas fa-envelope" />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="mb-3">
-                            <div className="input-group">
-                                <input
-                                    type="password"
-                                    className="form-control"
-                                    placeholder="Senha"
-                                />
-                                <div className="input-group-append">
-                                    <div className="input-group-text">
-                                        <span className="fas fa-lock" />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-    
-                    </form>
-                    <div className="social-auth-links text-center mt-2 mb-3">
+        <div className="login-page">
+            <div className="login-box">
+                <div className="card card-outline card-primary">
+                    <div className="card-header text-center">
+                            <b>PORTAL</b>
+                            <span> HPM</span>
                     </div>
-                    <p className="mb-1">
-                        esqueci a senha
-                    </p>
-                    <p className="mb-0">
-                        novo usuario
-                    </p>
+                    <div className="card-body login-card-body">
+                        <p className="login-box-msg">Insira abaixo suas credenciais para entrar no módulo HPM</p>
+                        <form >
+                            <div className="mb-3">
+                                <div className="input-group">
+                                    <input
+                                        type="email"
+                                        className="form-control"
+                                        placeholder="Usuário"
+                                    />
+                                    <div className="input-group-append">
+                                        <div className="input-group-text">
+                                            <span className="fas fa-envelope" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="mb-3">
+                                <div className="input-group">
+                                    <input
+                                        type="password"
+                                        className="form-control"
+                                        placeholder="Senha"
+                                    />
+                                    <div className="input-group-append">
+                                        <div className="input-group-text">
+                                            <span className="fas fa-lock" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </form>
+                        <div className="social-auth-links text-center mt-2 mb-3">
+                        </div>
+                        <p className="mb-1">
+                            esqueci a senha
+                        </p>
+                        <p className="mb-0">
+                            novo usuario
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
