0923a2162ab0e5bc93cb867bc9e2c8f8cb32bb20
93118180544 <urian.castro@gmail.com>
2021-03-18 09:03:27 -0300

    Alterações nos imports

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import Login from './modules/Login';
-<<<<<<< HEAD
 import Principal from './modules/principal';
 
 //JS
 import 'jquery/dist/jquery.min';
-=======
-import Principal from './modules/Principal';
-//CSS
-//JS
-import 'admin-lte/dist/js/adminlte.min';
->>>>>>> c9dcf3c8e5d28c7b64fa4850cf114a248b9ffae4
 import 'overlayscrollbars-react/dist/overlayscrollbars-react';
 
 class App extends Component {
