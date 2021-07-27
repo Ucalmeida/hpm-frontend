51510af27fabcc78f5269216926737c057678623
93118180544 <urian.castro@gmail.com>
2021-07-27 12:41:34 -0300

    Alteração Pessoas

--- src/paginas/cadastrar/Pessoa.js
+++ src/paginas/cadastrar/Pessoa.js
@@ -3,6 +3,7 @@ import {HttpVerbo, MSG} from "../../util/Constantes";
 import {BotaoSalvar, Card, Input, Pagina, Select} from "../../componentes";
 import {ExibirMensagem, xfetch} from "../../util";
 import MaskedInput from "../../componentes/form/MaskedInput";
+import { UseVisibilityToggler } from '../../componentes/UseVisibilityToggler';
 
 export default function Pessoa() {
     const [objeto, setObjeto] = useState(
@@ -21,7 +22,7 @@ export default function Pessoa() {
         nmCoren: null,
         nmCrefito: null,
         nmConter: null
-    })
+    });
 
     const pessoa = {
         nome: null,
@@ -86,7 +87,7 @@ export default function Pessoa() {
             nmCrefito: psf.nmCrefito,
             nmConter: psf.nmConter
         }});
-    },[psf]);
+    }, [psf]);
 
     const handleChange = (e) => {
         const {name, value} = e.target;
@@ -102,11 +103,113 @@ export default function Pessoa() {
         } else if(name === 'email') {
             setObjeto({...objeto, [name]: value.toLowerCase()});
         } else if(name === 'nmRegistroConselho' || name === 'nmCoren' || name === 'nmCrefito' || name === 'nmConter') {
-            setPsf({...psf, [name] : value});
+            setPsf({...psf, [name] : value.toUpperCase()});
         } else {
             setObjeto({...objeto, [name]: value});
         }
     }
+
+    const [ConselhoComponent, toggleConselhoVisibility] = UseVisibilityToggler(
+        <div className="col-lg-3">
+            <Input
+                type="text"
+                value={psf.nmRegistroConselho}
+                onChange={handleChange}
+                name="nmRegistroConselho"
+                placeholder="Registro"/>
+        </div>, false
+    );
+
+    const [CorenComponent, toggleCorenVisibility] = UseVisibilityToggler(
+        <div className="col-lg-3">
+            <Input
+                type="text"
+                value={psf.nmCoren}
+                onChange={handleChange}
+                name="nmCoren"
+                placeholder="Coren"/>
+        </div>, false
+    );
+
+    const [CrefitoComponent, toggleCrefitoVisibility] = UseVisibilityToggler(
+        <div className="col-lg-3">
+            <Input
+                type="text"
+                value={psf.nmCrefito}
+                onChange={handleChange}
+                name="nmCrefito"
+                placeholder="Crefito"/>
+        </div>, false
+    );
+
+    const [ConterComponent, toggleConterVisibility] = UseVisibilityToggler(
+        <div className="col-lg-3">
+            <Input
+                type="text"
+                value={psf.nmConter}
+                onChange={handleChange}
+                name="nmConter"
+                placeholder="Conter"/>
+        </div>, false
+    );
+
+
+    const [CardComponent, toggleCardVibility] = UseVisibilityToggler(
+        <Card titulo="Profissional de Saúde">
+            <div className="row form-group">
+                <div className="col-lg-12">
+                    <Select
+                        placeholder={"Especialidades"}
+                        funcao={handleEspecialidade}
+                        nome={"especialidades"}
+                        multiplo={true}
+                        url={"/hpm/especialidade/opcoes"}/>
+                </div>
+                <br/>
+                <br/>
+                <div className="form-check form-check-inline">
+                    <Input 
+                        type="checkbox"
+                        className="form-check-input"
+                        value='conselho'
+                        onClick={toggleConselhoVisibility}
+                        id="conselhoCheck"
+                        label="Registro do Conselho"/>
+                </div>
+                {ConselhoComponent}
+                <div className="form-check form-check-inline">
+                    <Input 
+                        type="checkbox"
+                        className="form-check-input"
+                        value='coren'
+                        onClick={toggleCorenVisibility}
+                        id="corenCheck"
+                        label="Registro do Coren"/>
+                </div>
+                {CorenComponent}
+                <div className="form-check form-check-inline">
+                    <Input 
+                        type="checkbox"
+                        className="form-check-input"
+                        value='crefito'
+                        onClick={toggleCrefitoVisibility}
+                        id="crefitoCheck"
+                        label="Registro do Crefito"/>
+                </div>
+                {CrefitoComponent}
+                <div className="form-check form-check-inline">
+                    <Input 
+                        type="checkbox"
+                        className="form-check-input"
+                        value='conter'
+                        onClick={toggleConterVisibility}
+                        id="conterCheck"
+                        label="Registro do Conter"/>
+                </div>
+                {ConterComponent}
+            </div>
+        </Card>, false
+    );
 
     return(
         <Pagina titulo="Cadastrar Pessoa">
@@ -230,62 +333,18 @@ export default function Pessoa() {
                                     nome={"sexo"}
                                     url={"/hpm/sexo/opcoes"}/>
                             </div>
-                            <div className="col-lg-3">
-                                <label>É Profissional da Saúde?</label>
-                                <Input type="checkbox"/>
+                            <div className="form-check form-check-inline">
+                                <Input 
+                                    type="checkbox"
+                                    className="form-check-input"
+                                    value='psf'
+                                    onClick={toggleCardVibility}
+                                    id="pfsCheck"
+                                    label="É Profissional da Saúde?"/>
                             </div>
                         </div>
                         <br/>
-                        <Card titulo="Profissional de Saúde">
-                            <div className="row form-group">
-                                <div className="col-lg-12">
-                                    <Select
-                                        placeholder={"Especialidades"}
-                                        funcao={handleEspecialidade}
-                                        nome={"especialidades"}
-                                        multiplo={true}
-                                        url={"/hpm/especialidade/opcoes"}/>
-                                </div>
-                                <br/>
-                                <br/>
-                                <div className="col-lg-3">
-                                    <Input
-                                        type="text"
-                                        value={psf.nmRegistroConselho}
-                                        onChange={handleChange}
-                                        name="nmRegistroConselho"
-                                        label="Registro do Conselho"
-                                        placeholder="Registro"/>
-                                </div>
-                                <div className="col-lg-3">
-                                    <Input
-                                        type="text"
-                                        value={psf.nmCoren}
-                                        onChange={handleChange}
-                                        name="nmCoren"
-                                        label="Registro do Coren"
-                                        placeholder="Coren"/>
-                                </div>
-                                <div className="col-lg-3">
-                                    <Input
-                                        type="text"
-                                        value={psf.nmCrefito}
-                                        onChange={handleChange}
-                                        name="nmCrefito"
-                                        label="Registro do Crefito"
-                                        placeholder="Crefito"/>
-                                </div>
-                                <div className="col-lg-3">
-                                    <Input
-                                        type="text"
-                                        value={psf.nmConter}
-                                        onChange={handleChange}
-                                        name="nmConter"
-                                        label="Registro do Conter"
-                                        placeholder="Conter"/>
-                                </div>
-                            </div>
-                        </Card>
+                        {CardComponent}
                         <div className="col-lg-15 text-lg-right mt-4 mb-4">
                             <BotaoSalvar onClick={enviar} />
                         </div>
---
+++ src/paginas/vincular/index.js
@@ -0,0 +1,13 @@
+import {SetorFuncao} from "./SetorFuncao";
+import {SetorFuncaoAcoes} from "./SetorFuncaoAcoes";
+import {SubSetor} from "./SubSetor";
+import {PerfilAcoes} from './PerfilAcoes';
+import {ProfissionalSaudeEspecialidade} from "./ProfissionalSaudeEspecialidade";
+
+export {
+    SetorFuncao,
+    SetorFuncaoAcoes,
+    SubSetor,
+    PerfilAcoes,
+    ProfissionalSaudeEspecialidade
+}
