1dee0ab58ba764532db06eff41533287f631b814
93118180544 <urian.castro@gmail.com>
2021-07-29 09:07:58 -0300

    Delete UseVisibilityToggler.js

--- src/componentes/UseVisibilityToggler.js
+++
@@ -1,7 +0,0 @@
-import React, { useState } from 'react'
-
-export const UseVisibilityToggler = (component, visibility = false) => {
-    const [visible, setVisible] = useState(() => visibility);
-    
-    return [visible ? component : null, () => setVisible((v) => !v)];
-}
