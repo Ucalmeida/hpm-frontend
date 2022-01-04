3dccd9bccaaf5b9a9fcb87f88928ecd1f4211cf4
urian.almeida <urian.almeida@wmw.com.br>
2022-01-04 09:45:19 -0300

    Inserção de um import para fazer funcionar os modais de aviso

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import 'overlayscrollbars-react/dist/overlayscrollbars-react';
 import 'admin-lte/dist/js/adminlte.min';
+import 'admin-lte/plugins/bootstrap/js/bootstrap.bundle.min';
 import 'admin-lte/dist/css/adminlte.min.css';
 import './componentes/pagina/menu.js'
 import App from './App';
