bb2269cd755cec7e33d282d37c7d17ef208d2cea
93118180544 <urian.castro@gmail.com>
2022-12-13 12:48:09 -0300

    Organização da página;

--- src/paginas/cadastrar/CadastrarEscala.js
+++ src/paginas/cadastrar/CadastrarEscala.js
@@ -76,58 +76,56 @@ export default function CadastrarEscala() {
             <div className="row">
                 <div className="col-lg-12">
                     <Card titulo="Cadastrar">
-                        <div className="col-lg-6">
-                            {/*<Input*/}
-                            {/*    type="text"*/}
-                            {/*    value={objeto.nome}*/}
-                            {/*    onChange={handleChange}*/}
-                            {/*    name="nome"*/}
-                            {/*    label="Nome"*/}
-                            {/*    placeholder="Nome" required/>*/}
-                            <label>Nome</label>
-                            <select className={"form-control col-lg-12"} name="mes" onChange={handleMes}>
-                                <option>Selecione...</option>
-                                <option value={"Janeiro"}>Janeiro</option>
-                                <option value={"Fevereiro"}>Fevereiro</option>
-                            </select>
-                            <select className={"form-control col-lg-12"} name="ano" onChange={handleAno}>
-                                <option>Selecione...</option>
-                                <option value={"2022"}>2022</option>
-                                <option value={"2023"}>2023</option>
-                            </select>
-                        </div>
-                        <div className="col-lg-6">
-                            <Input
-                                type="datetime-local"
-                                value={objeto.dataInicio}
-                                onChange={handleDtHrInicio}
-                                name="dataInicio"
-                                step="1"
-                                label="Data e hora início"
-                                placeholder="Data e hora"/>
-                        </div>
-                        <div className="col-lg-6">
-                            <Input
-                                type="datetime-local"
-                                value={objeto.dataTermino}
-                                onChange={handleDtHrTermino}
-                                name="dataTermino"
-                                step="1"
-                                label="Data e hora término"
-                                placeholder="Data e hora"/>
-                        </div>
-                        <div className="col-lg-2">
-                            <label>Tipo Escala</label>
-                            <select
-                                className="form-control"
-                                name="idStatus"
-                                value={objeto.idStatus}
-                                onChange={handleStatus}>
-                                <option hidden>Selecione...</option>
-                                {status.listaStatus.map((v, k) => {
-                                    return <option className="flex-fill" value={v.id} key={k}> {v.nome}</option>
-                                })}
-                            </select>
+                        <div className={"row"}>
+                            <div className="col-lg-6">
+                                <label>Mês</label>
+                                <select className={"form-control col-lg-12"} name="mes" onChange={handleMes}>
+                                    <option>Selecione...</option>
+                                    <option value={"Janeiro"}>Janeiro</option>
+                                    <option value={"Fevereiro"}>Fevereiro</option>
+                                </select>
+                            </div>
+                            <div className={"col-lg-6"}>
+                                <label>Ano</label>
+                                <select className={"form-control col-lg-12"} name="ano" onChange={handleAno}>
+                                    <option>Selecione...</option>
+                                    <option value={"2022"}>2022</option>
+                                    <option value={"2023"}>2023</option>
+                                </select>
+                            </div>
+                            <div className="col-lg-6">
+                                <Input
+                                    type="datetime-local"
+                                    value={objeto.dataInicio}
+                                    onChange={handleDtHrInicio}
+                                    name="dataInicio"
+                                    step="1"
+                                    label="Data e hora início"
+                                    placeholder="Data e hora"/>
+                            </div>
+                            <div className="col-lg-6">
+                                <Input
+                                    type="datetime-local"
+                                    value={objeto.dataTermino}
+                                    onChange={handleDtHrTermino}
+                                    name="dataTermino"
+                                    step="1"
+                                    label="Data e hora término"
+                                    placeholder="Data e hora"/>
+                            </div>
+                            <div className="col-lg-2">
+                                <label>Tipo Escala</label>
+                                <select
+                                    className="form-control"
+                                    name="idStatus"
+                                    value={objeto.idStatus}
+                                    onChange={handleStatus}>
+                                    <option hidden>Selecione...</option>
+                                    {status.listaStatus.map((v, k) => {
+                                        return <option className="flex-fill" value={v.id} key={k}> {v.nome}</option>
+                                    })}
+                                </select>
+                            </div>
                         </div>
                         <div className="col-lg-15 text-lg-right mt-4 mb-4">
                             <BotaoSalvar onClick={enviar} />
