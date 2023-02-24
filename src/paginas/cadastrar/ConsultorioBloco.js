894f79419456463517e485fff287452bd999e1cd
URIAN DE CASTRO ALMEIDA <93118180544@pmse.gov>
2023-02-24 09:14:06 -0300

    Ajustes

--- src/paginas/cadastrar/ConsultorioBloco.js
+++ src/paginas/cadastrar/ConsultorioBloco.js
@@ -1,8 +1,7 @@
-import React, {useEffect, useState} from "react";
-import {Botao, BotaoSalvar, Card, Input, Pagina, Select, Tabela} from "../../componentes";
-import {ExibirMensagem, xfetch} from "../../util";
-import {BOTAO, HttpVerbo, ICONE, MSG} from "../../util/Constantes";
-import { Logoff } from '../../util/Util';
+import React, { useEffect, useState } from "react";
+import { Botao, BotaoSalvar, Card, Input, Pagina, Select, Tabela } from "../../componentes";
+import { ExibirMensagem, xfetch } from "../../util";
+import { BOTAO, HttpVerbo, ICONE, MSG } from "../../util/Constantes";
 import EditarConsultorioBloco from '../editar/EditarConsultorioBloco';
 // ATUALIZAR: import ConsultoriosBlocoCard from "../../componentes/card/ConsultoriosBlocoCard"; -- Comitei
 
