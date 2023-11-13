67f43460507e480eca206463a95a5bf2aa7963f0
ROBERTO MATHEUS LIMA TORRES <07121450526@pmse.gov>
2023-11-13 10:16:41 -0300

    fix/ Ao Selecionar um campo de especialidade reiniciar a lista

--- src/paginas/recepcao/ListarPacientes.js
+++ src/paginas/recepcao/ListarPacientes.js
@@ -1,11 +1,12 @@
-import React, {useEffect, useState} from "react";
-import {Select} from "../../componentes/form";
-import {ExibirMensagem, xfetch} from "../../util";
-import {BOTAO, HttpVerbo, MSG} from "../../util/Constantes";
-import {Botao, Card, Pagina, Tabela} from "../../componentes";
+import React, { useEffect, useState } from "react";
+import { Select } from "../../componentes/form";
+import { ExibirMensagem, xfetch } from "../../util";
+import { BOTAO, HttpVerbo, MSG } from "../../util/Constantes";
+import { Botao, Card, Pagina, Tabela } from "../../componentes";
 
 export default function ListarPacientes() {
     const [objeto, setObjeto] = useState({});
+
 
     const [consultorio, setConsultorio] = useState({
         "idConsultorioBloco": null,
@@ -21,13 +22,13 @@ export default function ListarPacientes() {
         consultaSelecionada.idConsulta = consultaId;
         consultaSelecionada.idStatus = statusId;
         await xfetch('/hpm/consulta/alterar-status', consultaSelecionada, HttpVerbo.POST)
-            .then( json => {
-                    if(typeof json !== "undefined" ? json.status === "OK" : false) {
-                        ExibirMensagem('Consulta Alterada!', MSG.SUCESSO, '', '', '', '', listarPacientesPorData())
-                    }else{
-                        ExibirMensagem(json.message, MSG.ERRO)
-                    }
+            .then(json => {
+                if (typeof json !== "undefined" ? json.status === "OK" : false) {
+                    ExibirMensagem('Consulta Alterada!', MSG.SUCESSO, '', '', '', '', listarPacientesPorData())
+                } else {
+                    ExibirMensagem(json.message, MSG.ERRO)
                 }
+            }
             )
     }
 
@@ -35,18 +36,24 @@ export default function ListarPacientes() {
         consultaSelecionada.idConsulta = consultaId;
         consultaSelecionada.idStatus = statusId;
         await xfetch('/hpm/consulta/alterar-status', consultaSelecionada, HttpVerbo.POST)
-            .then( json =>{
-                    if(json.status === "OK"){
-                        ExibirMensagem('Consulta Cancelada!', MSG.SUCESSO, '', '', '', '', listarPacientesPorData())
-                    }else{
-                        ExibirMensagem(json.message, MSG.ERRO)
-                    }
+            .then(json => {
+                if (json.status === "OK") {
+                    ExibirMensagem('Consulta Cancelada!', MSG.SUCESSO, '', '', '', '', listarPacientesPorData())
+                } else {
+                    ExibirMensagem(json.message, MSG.ERRO)
                 }
+            }
             )
     }
 
     const selecionarEspecialidade = (e) => {
-        setObjeto({...objeto, idEspecialidade: e.value});
+        const novaEspecialidade = e.value;
+        setObjeto({
+            idEspecialidade: novaEspecialidade,
+            profissionais: [],
+            consultoriosBloco: [],
+            consultas: [],
+        });
     }
 
     const selecionarProfissionalSaude = (e) => {
@@ -56,6 +63,9 @@ export default function ListarPacientes() {
 
     const selecionarConsultorioBloco = (e) => {
         consultorio.idConsultorioBloco = Number(e.target.value);
+        if (consultorio.idStatus !== null) {
+            listarPacientesPorData();
+        }
     }
 
     const selecionarStatus = (e) => {
@@ -68,54 +78,58 @@ export default function ListarPacientes() {
     }
 
     const listarProfissionalPorEspecialidade = () => {
-        if(typeof objeto.idEspecialidade !== 'undefined') {
-            xfetch('/hpm/profissionalSaude/' + objeto.idEspecialidade + '/opcoes',{}, HttpVerbo.GET)
-                .then(res => res.json())
-                .then(json => {
-                        setObjeto({...objeto, profissionais: json.resultado});
+        if (typeof objeto.idEspecialidade !== "undefined") {
+            xfetch("/hpm/profissionalSaude/" + objeto.idEspecialidade + "/opcoes", {}, HttpVerbo.GET)
+                .then((res) => res.json())
+                .then((json) => {
+                    if (json.resultado) {
+                        setObjeto({ ...objeto, profissionais: json.resultado });
+                    } else {
+                        // Limpar a lista de profissionais se não houver resultado
+                        setObjeto({ ...objeto, profissionais: [] });
                     }
-                )
+                });
         }
-    }
+    };
 
     const listarDatasPorEspecialidadeProfissionalSaude = () => {
-        setObjeto({...objeto, consultoriosBloco: []})
+        setObjeto({ ...objeto, consultoriosBloco: [] })
         xfetch('/hpm/consultorioBloco/' + objeto.idEspecialidade + '/' + objeto.idProfissionalSaude + '/opcoes', {}, HttpVerbo.GET)
             .then(res => res.json())
             .then(json => {
-                    setObjeto({...objeto, consultoriosBloco: json.resultado})
-                }
+                setObjeto({ ...objeto, consultoriosBloco: json.resultado })
+            }
             )
     }
 
     const listarPacientesPorData = () => {
         xfetch('/hpm/consulta/pesquisar/consultorio-status', consultorio, HttpVerbo.POST)
             .then(response => {
-                    if (response.status === "OK"){
-                        setObjeto({...objeto, consultas: response.resultado})
-                    } else{
-                        setObjeto({...objeto, consultas: []})
-                        ExibirMensagem("Não existe resultados para essa pesquisa!", MSG.ALERTA)
-                    }
+                if (response.status === "OK") {
+                    setObjeto({ ...objeto, consultas: response.resultado })
+                } else {
+                    setObjeto({ ...objeto, consultas: [] })
+                    ExibirMensagem("Não existe resultados para essa pesquisa!", MSG.ALERTA)
                 }
+            }
             )
             .catch(error => console.log(error))
     }
 
-    useEffect( () => {
+    useEffect(() => {
         listarProfissionalPorEspecialidade();
     }, [objeto.idEspecialidade]);
 
     const colunas = [
-        {text: "Paciente"},
-        {text: "Telefone"},
-        {text: "Atendimento"},
-        {text: "Ordem"},
-        {text: "Ações" }
+        { text: "Paciente" },
+        { text: "Telefone" },
+        { text: "Atendimento" },
+        { text: "Ordem" },
+        { text: "Ações" }
     ]
 
     const dados = () => {
-        return(
+        return (
             typeof objeto.consultas !== 'undefined' ? objeto.consultas.map((consulta, indice) => {
                 let corBotao = BOTAO.COR.PRIMARIO;
                 let nomeBotao = 'Confirmar';
@@ -135,13 +149,15 @@ export default function ListarPacientes() {
                     'atendimento': consulta.nmStatus,
                     'ordem': consulta.numOrdem,
                     'acoes': <div>
-                                <Botao cor={corBotao} onClick={() => handleBtnConfirmar(consulta.id, Number("6"))} value={consulta.id} disabled={isDesabilitado}>{nomeBotao}</Botao>
-                                <Botao cor={BOTAO.COR.ALERTA} onClick={() => handleBtnCancelar(consulta.id, Number("8"))} value={consulta.id} disabled={isDesabilitado}>Cancelar</Botao>
-                             </div>
+                        <Botao cor={corBotao} onClick={() => handleBtnConfirmar(consulta.id, Number("6"))} value={consulta.id} disabled={isDesabilitado}>{nomeBotao}</Botao>
+                        <Botao cor={BOTAO.COR.ALERTA} onClick={() => handleBtnCancelar(consulta.id, Number("8"))} value={consulta.id} disabled={isDesabilitado}>Cancelar</Botao>
+                    </div>
                 })
             }) : ''
         )
     }
+
+    console.log(objeto);
 
     let consultaBloco = objeto.consultoriosBloco;
 
@@ -173,21 +189,21 @@ export default function ListarPacientes() {
                             </div>
                             <div className="col-lg-3">
                                 <label>Data - Hora</label>
-                                <br/>
+                                <br />
                                 <select
                                     className="form-control"
                                     name="idConsultorioBloco"
                                     value={objeto.idConsultorioBloco}
                                     onChange={selecionarConsultorioBloco}>
                                     <option hidden>Selecione...</option>
-                                    {typeof(consultaBloco) !== 'undefined' ? consultaBloco.map((v, k) => {
+                                    {typeof (consultaBloco) !== 'undefined' ? consultaBloco.map((v, k) => {
                                         return <option className="flex-fill" value={v.valor} key={k}> {v.texto}</option>
                                     }) : ''}
                                 </select>
                             </div>
                             <div className="col-lg-3">
                                 <label>Status</label>
-                                <br/>
+                                <br />
                                 <Select
                                     url={"/hpm/status/objeto-opcoes/" + 20}
                                     nome={"idStatus"}
